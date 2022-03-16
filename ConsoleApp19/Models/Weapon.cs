10b5aef5e7ff4b76fee470ee6d74c41dec2a1084
KenanNagiyev <788lnmp@code.edu.az>
2022-03-16 10:37:58 +0400

    new file

---
+++ ConsoleApp19/Models/Weapon.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp19.Models
+{
+    class Weapon
+    {
+        private int _bulletCap;
+        private int _bulletCount;
+        private double _time;
+        private bool _auto = true;
+        public int bulletcap
+        {
+            get
+            {
+                return _bulletCap;
+            }
+            set
+            {
+                if (value < 0 || value < bulletcount)
+                {
+
+                    value = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("daxil edilen inputsefdir");
+                }
+                _bulletCap = value;
+            }
+        }
+        public int bulletcount
+        {
+            get
+            {
+                return _bulletCount;
+            }
+            set
+            {
+                if (value<0 || value>bulletcap)
+                {
+                    value = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("daxil edilen input sefdir");
+                }
+                _bulletCount = value;
+            }
+            
+        }
+        public double time
+        {
+            get
+            {
+                return _bulletCount;
+            }
+            set
+            {
+                if (value<=0)
+                {
+                    value = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("daxil edilen input sefdir");
+                }
+                _time = value;
+            }
+        }
+        public bool auto
+        {
+            get
+            {
+                return _auto;
+            }
+            set
+            {
+                if (value == true)
+                {
+                    value = true;
+                }
+                else
+                {
+                    value = false;
+                }
+                _auto = value;
+            }
+            
+             
+        }
+        public Weapon()
+        {
+
+        }
+
+
+        public Weapon(int bulletCap, int bulletCount, double time, bool auto)
+        {
+            this._bulletCap = bulletCap;
+            this._bulletCount = bulletCount;
+            this._time = time;
+            this._auto = auto;
+            if (bulletCount > bulletCap)
+            {
+                Console.WriteLine("Xeberdarliq:gulle sayi tutumdan cox ola bilmez");
+            }
+
+        }
+        public void GetInfo()
+        {
+            Console.WriteLine($"gulle tutumu:{ _bulletCap}");
+            Console.WriteLine($"gulle sayi:{ _bulletCount}");
+            Console.WriteLine($"vaxti:{ _time}");
+        }
+        public void Shoot()
+        {
+
+
+            if (_bulletCount <= 0)
+            {
+                Console.WriteLine("Gulle yoxdur");
+            }
+            else if (_auto == true)
+            {
+
+                _bulletCount--;
+
+                if (_bulletCount > 0)
+                {
+                    _bulletCount--;
+
+                }
+                else
+                {
+                    Console.WriteLine("Gulle yoxdur");
+                }
+
+            }
+        }
+
+        public void Fire()
+        {
+            double shootbullets = 0;
+            if (_bulletCount == 0)
+            {
+                Console.WriteLine("gulle  yoxdur");
+                return;
+            }
+            if (_auto)
+            {
+                while (_bulletCount > 0)
+                {
+                    _bulletCount--;
+                    shootbullets++;
+                    Console.WriteLine(shootbullets);
+                }
+            }
+            else
+            {
+                while (_bulletCount > 0)
+                {
+                    _bulletCount--;
+                    shootbullets++;
+                    Console.WriteLine(shootbullets);
+                }
+            }
+            shootbullets *= (_bulletCap / time);
+            Console.WriteLine($"gulle bu qeder vaxta atildi:{shootbullets}");
+        }
+        public void GetRemainBulletCount()
+        {
+            Console.WriteLine(_bulletCap - _bulletCount);
+        }
+        public void Reload()
+        {
+            if (_bulletCap - _bulletCount == 0)
+            {
+                Console.WriteLine("doludur");
+            }
+            else
+            {
+                Console.WriteLine(_bulletCap - _bulletCount);
+                _bulletCount = _bulletCap;
+            }
+        }
+    }
+}
---
+++ ConsoleApp19/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using ConsoleApp19.Models;
+namespace ConsoleApp19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Weapon w = new Weapon(10, 4, 4, true);
+
+            int choose = 0;
+            while (choose != 6)
+            {
+                Console.WriteLine("choose");
+                choose = Convert.ToInt32(Console.ReadLine());
+                switch (choose)
+                {
+                    case 0:
+                        {
+                            w.Shoot();
+                        }
+                        break;
+                    case 1:
+                        {
+                            w.Fire();
+                        }
+                        break;
+                    case 2:
+                        {
+                            w.GetRemainBulletCount();
+                        }
+                        break;
+                    case 3:
+                        {
+                            w.Reload();
+                        }
+                        break;
+                    case 4:
+                        {
+                            w.GetInfo();
+                        }
+                        break;
+
+                    default:
+                        {
+                            Console.WriteLine("sefdir");
+                        }
+                        break;
+                }
+
+                }
+            }
+        }
+}
